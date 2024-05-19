3c66ebe803e9b5b78b6f284f6e6bd736b0ff4a2d
Martineaux Galaad <galaad.martineaux@universite-paris-saclay.fr>
2024-05-19 12:54:00 +0200

    Added numerical sorting, and fixed link to img not appearing.

--- index.php
+++ index.php
@@ -9,6 +9,11 @@ if (is_null($_GET["folder"])) {
 
 	$dir = $_GET["folder"];
 	$files1 = scandir($dir);
+	// Remove '.' and '..' from the array
+	$files1 = array_diff($files1, array('.', '..'));
+
+	// Sort files in natural order
+	natsort($files1);
 	foreach ($files1 as $key => $img) {
 		if ($img == '.' || $img == '..') continue;
 	
@@ -16,6 +21,8 @@ if (is_null($_GET["folder"])) {
 		if (is_dir($dir . "/" . $img)) {
 			echo "<a href='index.php?folder=" . $dir . "/" . $img . "'>" . $img . "</a><br/>";
 		} else {
+			$img = str_replace("'", "&#x27;", $img);
+			$img = str_replace("\"", '&#x22;', $img);
 			echo "<img src='" . $dir . "/". $img . "'/><br>";
 		}
 	}
