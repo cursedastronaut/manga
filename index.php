05a8647a8148455af619e6eb73d07469d14ac4fd
Martineaux Galaad <galaad.martineaux@universite-paris-saclay.fr>
2024-05-19 13:41:42 +0200

    Updated look

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 if (is_null($_GET["folder"])) {
+	echo "<div class='flex-row'>";
 	$files1 = scandir("content");
 	foreach ($files1 as $key => $sub) {
-		if ($sub == '.') continue;
-		echo "<a href='index.php?folder=" . "content/" . $sub . "'>" . $sub . "</a><br/>";
+		if ($sub == '.' || $sub == '..' || str_contains($sub, "preview-")) continue;
+		$previewExists = file_exists("content/.preview-" . $sub . ".jpg");
+		if ($previewExists)
+			echo "<div class='preview'><img src='content/.preview-" . $sub . ".jpg'/>";
+		echo "<a href='index.php?folder=" . "content/" . $sub . "'>" . $sub . "</a>";
+		if ($previewExists)
+			echo "</div>";
+		else
+			echo "<br/>";
 	}
+	echo "</div>";
 } else {
-
 	$dir = $_GET["folder"];
 	$files1 = scandir($dir);
 	// Remove '.' and '..' from the array
@@ -23,14 +31,27 @@ if (is_null($_GET["folder"])) {
 		} else {
 			$img = str_replace("'", "&#x27;", $img);
 			$img = str_replace("\"", '&#x22;', $img);
-			echo "<img src='" . $dir . "/". $img . "'/><br>";
+			echo "<img class='page' src='" . $dir . "/". $img . "'/><br>";
 		}
 	}
 }
 ?>
 
 <style>
-	body {
+	.flex-row {
 		text-align: center;
+		display: flex;
+		flex-flow: row;
+	}
+
+	.page {
+		max-width: 100%;
+	}
+
+	.preview {
+		max-height: 256px;
+		max-width: calc(10% - 16px);
+		display: flex;
+		flex-flow: column;
 	}
 </style>
